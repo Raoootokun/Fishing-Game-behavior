62df1fe239657f18643af9c0782ef6dd974eb7ff
H4RUYAN <by2mwnqwg7@privaterelay.appleid.com>
2026-01-22 22:23:56 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { world, system, Player, } from "@minecraft/server";
 import { WorldLoad } from "./lib/WorldLoad" 
 import "./ExHud/ExHud";
 
+world.sendMessage("reload!");
 
 WorldLoad.subscribe(() => {
     
